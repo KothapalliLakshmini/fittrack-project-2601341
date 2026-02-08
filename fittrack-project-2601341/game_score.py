4c3a1b5b6fdaa7f607d5a251b4bad9c65374a6c8
DevStudent2601341 <student2601341@university.edu>
2026-02-08 20:32:50 +0530

    Player Score Calculator

---
+++ fittrack-project-2601341/game_score.py
@@ -0,0 +1,10 @@
+player_Name = input('''Enter the player's name : ''')
+games_played = int(input('''Enter No.of Games Played : '''))
+total_score_acheived = int(input('''Enter total score achieved : '''))
+average_score = total_score_acheived/games_played
+
+print(f"Player: {player_Name}")
+print(f"Games Played: {games_played}")
+print(f"Total Score: {total_score_acheived}")
+print(f"Average Score: {average_score}")
+
